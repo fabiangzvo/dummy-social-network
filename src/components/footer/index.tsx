87a33590bacff94e78b04c64068fd586ec4d6e61
fabiangzvo <faguzman.97@gmail.com>
2024-02-05 10:40:35 -0500

    add responsive support in login

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,20 +1,28 @@
 import SocialNetwork from "@components/socialNetwork";
 import Text from "@components/text";
+import { useMediaQuery } from "@hooks/useMediaQuery";
 
 import styles from "./style.module.css";
 
 function Footer() {
+  const isLg = useMediaQuery("(max-width: 1024px)");
+  const isMd = useMediaQuery("(max-width: 768px)");
+
   return (
-    <footer className={styles.container}>
-      <div className={styles.leftContent}>
-        <Text fontSize="xl" className={styles.name}>
-          Fabián Guzmán Otavo.
+    <footer className={isMd ? styles.mdContainer : styles.container}>
+      <div className={styles.leftContent} style={{ order: isMd ? 2 : 1 }}>
+        <Text fontSize={isLg ? "lg" : "xl"} className={styles.name}>
+          Dummy app
         </Text>
-        <Text fontSize="lg" className={styles.copyRight}>
+        <Text fontSize={isLg ? "sm" : "lg"} className={styles.copyRight}>
           All rights reserved © 2024
         </Text>
       </div>
-      <div>
+      <div style={{ order: isMd ? 1 : 2 }}>
+        <Text fontSize={isLg ? "sm" : "lg"} fontWeight="bold">
+          Follow me:
+        </Text>
+        &emsp;
         <SocialNetwork />
       </div>
     </footer>
